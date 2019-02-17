f0217782112a27a7f8813981e9d0b609aed928e2
Bartosz Szczecinski <btm@anfo.pl>
2019-02-17 18:39:51 +0100

    15 init

--- ZADANIE.md
+++ ZADANIE.md
@@ -1,10 +1,12 @@
-# #14 Przekazywanie danych do rodzica
+# #15 Przekazywanie danych do rodzica
 
 Wiemy już jak przekazać dane z rodzica do dziecka - przekazywanie ich w drugą stronę jest nieco bardziej skomplikowane.
 
 ## Zadanie
 
 - zmodyfikuj komponent `App` tak, aby to on wywoływał `alert` z wartością wpisaną do `MessageForm`
+- pamiętaj o nie przekazywaniu "pustych" danych
+- nie wywołuj metody jeżeli nie została ona przekazana!
 
 ## Bonus
 
--- src/index.test.js
+++ src/index.test.js
@@ -15,3 +15,26 @@ test("Próba wysłania formularza wyświetla wpisane w niego dane", () => {
 
   expect(global.alert).toHaveBeenCalledWith("test alert");
 });
+
+test("Pusty formularz nie przesyła danych do rodzica", () => {
+  jest.restoreAllMocks();
+  jest.spyOn(global, "alert").mockImplementation(() => {});
+  const App = require("./App").App;
+  const { container } = render(<App data={[]} />);
+  fireEvent.submit(container.querySelector("form"));
+
+  expect(global.alert).not.toHaveBeenCalled();
+});
+
+test("Brak callbacku nie powoduje błędu", () => {
+  jest.restoreAllMocks();
+  jest.spyOn(global, "alert").mockImplementation(() => {});
+  const MessageForm = require("./MessageForm").MessageForm;
+  const { container } = render(<MessageForm time={new Date()} />);
+  fireEvent.change(container.querySelector("input"), {
+    target: {
+      value: "test alert"
+    }
+  });
+  fireEvent.submit(container.querySelector("form"));
+});
