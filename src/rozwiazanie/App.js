afc6d3949b4707451b5d481c85cbad132eba2a5e
Bartosz Szczecinski <btm@anfo.pl>
2019-02-19 05:41:54 +0100

    05 Dodane rozwiazanie

---
+++ src/rozwiazanie/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Message } from "./Message";
+
+export const App = () => (
+  <React.Fragment>
+    <Message />
+    <Message />
+    <Message />
+  </React.Fragment>
+);
---
+++ src/rozwiazanie/Message.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Message = props => (
+  <div>
+    <span>Twoje Imię</span>
+    <time>12:00</time>
+    <p>To jest przykładowa wiadomość :)</p>
+  </div>
+);
