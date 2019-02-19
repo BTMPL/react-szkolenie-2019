a1be57383a4904597501c184f6d8c89cd84ac47c
Bartosz Szczecinski <btm@anfo.pl>
2019-02-19 05:53:32 +0100

    09 Dodane propTypes

--- src/rozwiazanie/App.js
+++ src/rozwiazanie/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Message } from "./Message";
 
 export const App = ({ data }) =>
   data.map(item => (
     <Message userName={item.userName} time={item.time} message={item.message} />
   ));
+
+App.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape(Message.propTypes))
+};
