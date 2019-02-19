460e1d6c8706881cc885345ba6a43e6239e4a291
Bartosz Szczecinski <btm@anfo.pl>
2019-02-19 05:39:28 +0100

    03 Dodane rozwiazanie

---
+++ src/rozwiazanie/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+export const App = () => (
+  <p>
+    Witaj na <b>Warsztatach React</b>!
+  </p>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
