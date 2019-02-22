d082f09b1d9535e7b2a0f0d0a37d640ecc4fdfa9
Bartosz Szczecinski <baphemot@gmail.com>
2019-02-22 09:04:10 +0100

    Rozwiazanie 13

---
+++ src/rozwiazanie/MessageForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class MessageForm extends React.Component {
+  state = { value: "" };
+
+  render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (this.state.value === "") return;
+
+          alert(this.state.value);
+        }}
+      >
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={e => {
+            this.setState({
+              value: e.target.value
+            });
+          }}
+        />
+      </form>
+    );
+  }
+}
