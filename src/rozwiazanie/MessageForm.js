dcf2e2945c811c413d6aa926f0b01bece740270d
Bartosz Szczecinski <baphemot@gmail.com>
2019-02-22 09:14:18 +0100

    Add bonus 13, 14

--- src/rozwiazanie/MessageForm.js
+++ src/rozwiazanie/MessageForm.js
@@ -3,6 +3,10 @@ import React from "react";
 export class MessageForm extends React.Component {
   state = { value: "" };
 
+  static propTypes = {
+    onMessage: PropTypes.func
+  };
+
   render() {
     return (
       <form
@@ -11,6 +15,9 @@ export class MessageForm extends React.Component {
           if (this.state.value === "") return;
 
           this.props.onMessage && this.props.onMessage(this.state.value);
+          this.setState({
+            value: ""
+          });
         }}
       >
         <input
