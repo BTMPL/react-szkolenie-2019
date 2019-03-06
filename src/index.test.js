69ed8525eb7db086fe7d6dfec563725a8650b1cf
Bartosz Szczecinski <btm@anfo.pl>
2019-03-06 21:19:54 +0100

    25 init

--- src/index.test.js
+++ src/index.test.js
@@ -1,50 +1,17 @@
-import React from "react";
-import { render, wait, fireEvent, cleanup, act } from "react-testing-library";
+import { renderHook, cleanup } from "react-hooks-testing-library";
 
 afterEach(() => {
   cleanup();
   jest.clearAllMocks();
 });
 
-test("komponent ChatProvider zostały przetworzony na funkcję", () => {
+test("hook useChat zwraca poprawne dane", () => {
   jest.mock("./api");
-  const ChatProvider = require("./providers/chat.js").ChatProvider;
-  expect(ChatProvider.toString().indexOf("class")).toBe(-1);
-});
+  const useChat = require("./providers/chat").useChat;
 
-test("komponent chatProvider poprawnie wywołuje i anuluje subskrypcję", async () => {
-  jest.useFakeTimers();
-  jest.mock("./api");
-  const api = require("./api").api;
-  const ChatProvider = require("./providers/chat.js").ChatProvider;
-  const spy = jest.fn(({ isLoading, data }) => {
-    return !isLoading ? data.map(i => i.message).join("") : null;
-  });
+  const { result } = renderHook(() => useChat());
 
-  let getByText, unmount;
-  act(() => {
-    const ret = render(<ChatProvider>{spy}</ChatProvider>);
-    unmount = ret.unmount;
-    getByText = ret.getByText;
-    jest.advanceTimersByTime(1000);
-  });
-
-  await wait(() => getByText(/Test string/));
-
-  expect(spy).toHaveBeenCalledWith(
-    expect.objectContaining({
-      isLoading: false,
-      data: expect.any(Array),
-      create: expect.any(Function)
-    })
-  );
-
-  expect(api.get.mock.calls.length).toBe(1);
-
-  act(() => {
-    unmount();
-    jest.advanceTimersByTime(1000);
-  });
-
-  expect(api.get.mock.calls.length).toBe(1);
+  expect(result.current.data).toBeUndefined();
+  expect(typeof result.current.create).toBe(typeof Function);
+  expect(result.current.isLoading).toBe(true);
 });
