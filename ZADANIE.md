24a1e82406bb0b81e60ebd2f126cea0e0949c3a9
Bartosz Szczecinski <btm@anfo.pl>
2019-03-06 19:59:55 +0100

    24 init

--- ZADANIE.md
+++ ZADANIE.md
@@ -5,6 +5,19 @@
 - Zmień komponent `ChatProvider` na komponent oparty o funkcje
   - metody lifecycle zastąp wywołaniem `React.useEffect()`
 
+## Uwaga
+
+Jeżeli w testach zobaczysz komunikat
+
+```
+Warning: An update to ChatProvider inside a test was not wrapped in act(...)
+```
+
+nie znaczy to, że w kodzie jest błąd. Błąd ten jest znany, i na chwilę obecną nie jest rozwiązany:
+
+https://github.com/kentcdodds/react-testing-library/issues/281
+https://github.com/facebook/react/issues/14769
+
 ## Podpowiedź
 
 - `useEffect` jako drugi parametr przyjmuje tablicę której _wartości_ zostaną porównane i jeżeli nie uległy zmianie, efekt nie zostanie wywołany
--- src/index.test.js
+++ src/index.test.js
@@ -1,45 +1,50 @@
 import React from "react";
-import { render, wait, fireEvent, cleanup } from "react-testing-library";
+import { render, wait, fireEvent, cleanup, act } from "react-testing-library";
 
 afterEach(() => {
   cleanup();
   jest.clearAllMocks();
 });
 
-describe("ChatProvider", () => {
-  test("renderuje przekazane dziecko z odpowiednimi danymi", () => {
-    jest.mock("./api");
-    const ChatProvider = require("./providers/chat.js").ChatProvider;
-    const spy = jest.fn(() => null);
-    render(<ChatProvider>{spy}</ChatProvider>);
-
-    expect(spy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        isLoading: true,
-        data: undefined,
-        create: expect.any(Function)
-      })
-    );
-  });
+test("komponent ChatProvider zostały przetworzony na funkcję", () => {
+  jest.mock("./api");
+  const ChatProvider = require("./providers/chat.js").ChatProvider;
+  expect(ChatProvider.toString().indexOf("class")).toBe(-1);
 });
 
-describe("Aplikacja", () => {
-  test("Po zalogowaniu aplikacja pokazuje nagłówek, po pobraniu danych, nagłówek nie ulega re-renderowaniu", async () => {
-    jest.mock("./api");
-
-    const App = require("./App").App;
-    const { getByText, container } = render(<App />);
-
-    fireEvent.change(container.parentNode.querySelector("input"), {
-      target: {
-        value: "Bartek"
-      }
-    });
-    fireEvent.click(getByText(/Zaloguj/));
-    const h1 = container.querySelector("h1").innerText;
-
-    await wait(() => getByText(/Test string/));
-
-    expect(container.querySelector("h1").innerText).toBe(h1);
+test("komponent chatProvider poprawnie wywołuje i anuluje subskrypcję", async () => {
+  jest.useFakeTimers();
+  jest.mock("./api");
+  const api = require("./api").api;
+  const ChatProvider = require("./providers/chat.js").ChatProvider;
+  const spy = jest.fn(({ isLoading, data }) => {
+    return !isLoading ? data.map(i => i.message).join("") : null;
   });
+
+  let getByText, unmount;
+  act(() => {
+    const ret = render(<ChatProvider>{spy}</ChatProvider>);
+    unmount = ret.unmount;
+    getByText = ret.getByText;
+    jest.advanceTimersByTime(1000);
+  });
+
+  await wait(() => getByText(/Test string/));
+
+  expect(spy).toHaveBeenCalledWith(
+    expect.objectContaining({
+      isLoading: false,
+      data: expect.any(Array),
+      create: expect.any(Function)
+    })
+  );
+
+  expect(api.get.mock.calls.length).toBe(1);
+
+  act(() => {
+    unmount();
+    jest.advanceTimersByTime(1000);
+  });
+
+  expect(api.get.mock.calls.length).toBe(1);
 });
